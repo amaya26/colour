f25476e74bbdff1ca174e07c6261ff89447469a6
amaya26 <165841780+amaya26@users.noreply.github.com>
2025-03-17 14:29:13 +1300

    Add files via upload

--- 05_ststs_component_v1.py
+++ 05_ststs_component_v1.py
@@ -82,6 +82,25 @@ class Play:
     """
 
     def __init__(self, how_many):
+        self.rounds_won = IntVar()
+
+        # lists for stats component
+
+        # highest score test data
+        # self.all_scores_list = [20, 20, 20, 16, 19]
+        # self.all_high_score_list = [20, 20, 20, 16, 19]
+        # self.rounds_won.set(5)
+
+        # lowest score test data
+        # self.all_scores_list = [0, 0, 0, 0, 0]
+        # self.all_high_score_list = [20, 20, 20, 16, 19]
+        # self.rounds_won.set(0)
+
+        # random score test data
+        self.all_scores_list = [0, 15, 16, 0, 16]
+        self.all_high_score_list = [20, 19, 18, 20, 20]
+        self.rounds_won.set(3)
+
         self.play_box = Toplevel()
 
         self.game_frame = Frame(self.play_box)
@@ -91,49 +110,62 @@ class Play:
                                    font=("Arial", "16", "bold"), pady=5, padx=5)
         self.heading_label.grid(row=0)
 
-        self.to_help_button = Button(self.game_frame, text="Hints",
+        self.stats_button = Button(self.game_frame, text="Stats",
                                      font=("Arial", "14", "bold"),
                                      fg="#FFFFFF", bg="#FF8000", width=15,
                                      padx=10, pady=10,
-                                     command=self.to_hints)
-        self.to_help_button.grid(row=1)
+                                     command=self.to_stats)
+        self.stats_button.grid(row=1)
 
-    def to_hints(self):
+    def to_stats(self):
         """
-        Displays hints for playing the game
-        :return:
+        retrieves everything we need to display the game
         """
-        DisplayHints(self)
+
+        # important: retrieve number of rounds
+        # won as a number (rather than the 'self' container)
+        rounds_won = self.rounds_won.get()
+        stats_bundle = [rounds_won, self.all_scores_list,
+                        self.all_high_score_list]
+
+        Stats(self, stats_bundle)
 
 
-class DisplayHints:
+class Stats:
     """
-    Displays hints for Colour quest game
+    Displays stats for Colour quest game
     """
 
-    def __init__(self, partner):
-        # setup dialogue box and background colour
-        background = "#ffe6cc"
-        self.help_box = Toplevel()
+    def __init__(self, partner, all_stats_info):
+
+        # extract information from master list
+        rounds_won = all_stats_info[0]
+        user_scores = all_stats_info[1]
+        high_scores = all_stats_info[2]
+
+        # sort user scores to find high score
+        user_scores.sort()
+
+        self.stats_box = Toplevel()
 
         # disable help button
-        partner.to_help_button.config(state=DISABLED)
+        partner.stats_button.config(state=DISABLED)
 
         # If users press cross at top, closes help and
         # 'releases' help button
-        self.help_box.protocol('WM_DELETE_WINDOW',
-                               partial(self.close_help, partner))
+        self.stats_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_stats, partner))
 
-        self.help_frame = Frame(self.help_box, width=300,
+        self.stats_frame = Frame(self.stats_box, width=300,
                                 height=200)
-        self.help_frame.grid()
+        self.stats_frame.grid()
 
-        self.help_heading_label = Label(self.help_frame,
+        self.stats_heading_label = Label(self.stats_frame,
                                         text="Help / Info",
                                         font=("Arial", "14", "bold"))
-        self.help_heading_label.grid(row=0)
+        self.stats_heading_label.grid(row=0)
 
-        help_text = "To use the program, simply enter the temperature" \
+        stats_text = "To use the program, simply enter the temperature" \
                     " you wish to convert and then choose to convert " \
                     "to either degrees Celsius (centigrade) or " \
                     "Fahrenheit. \n\n" \
@@ -146,16 +178,16 @@ class DisplayHints:
                     "calculation history and export it to a text " \
                     "file, please click the 'History / Export' button."
 
-        self.help_text_label = Label(self.help_frame,
-                                     text=help_text, wraplength=350,
+        self.stats_text_label = Label(self.stats_frame,
+                                     text=stats_text, wraplength=350,
                                      justify="left")
-        self.help_text_label.grid(row=1, padx=10)
+        self.stats_text_label.grid(row=1, padx=10)
 
-        self.dismiss_button = Button(self.help_frame,
+        self.dismiss_button = Button(self.stats_frame,
                                      font=("Arial", "12", "bold"),
                                      text="Dismiss", bg="#CC6600",
                                      fg="#FFFFFF",
-                                     command=partial(self.close_help, partner))
+                                     command=partial(self.close_stats, partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
 
         # List and loop to set background colour on
